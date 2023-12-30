0eb099f3e15fb394d5a7e8af90adf9c4880cc3cb
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-12-30 01:04:16 +0100

    feat: initial commit

---
+++ src/guard.ts
@@ -0,0 +1,58 @@
+import type { RequestContext } from "cargo/http/request.ts";
+import type { Middleware, Next } from "cargo/middleware/middleware.ts";
+import { UnauthorizedException } from "cargo/http/exceptions/unauthorized-exception.ts";
+
+export interface Instructions<T> {
+  allow: (entity: T) => void;
+  deny: (reason: string) => void;
+}
+
+export interface Strategy<T> {
+  name: string;
+  authenticate: (
+    ctx: RequestContext,
+    instructions: Instructions<T>,
+  ) => Promise<void> | void;
+}
+
+function authentication<T>(
+  { authenticate }: Strategy<T>,
+  ctx: RequestContext,
+): Promise<T> {
+  return new Promise<T>((resolve, reject) => {
+    const attempt = authenticate(ctx, { allow: resolve, deny: reject });
+    if (attempt instanceof Promise) {
+      attempt.catch((e: Error) => {
+        reject(e.message);
+      });
+    }
+  });
+}
+
+const strategies: Strategy<unknown>[] = [];
+
+function protectWith<T>(strategyName: string): Middleware {
+  const strategy = <Strategy<T> | undefined> (
+    strategies.find((strategy) => strategy.name == strategyName)
+  );
+  if (strategy) {
+    return async (ctx: RequestContext, next: Next): Promise<Response> => {
+      try {
+        ctx.auth = await authentication<T>(strategy, ctx);
+      } catch (message) {
+        throw new UnauthorizedException(message);
+      }
+      return next(ctx);
+    };
+  }
+  throw Error("Strategy not defined!");
+}
+
+function strategy<T>(strategy: Strategy<T>) {
+  strategies.push(strategy);
+}
+
+export const Guard = {
+  protectWith,
+  strategy,
+};
